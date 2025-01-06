ff5f0043e7f26c2156c9a82ff66190e2ef272b65
DaniilBrylev <dan.brylev38@gmail.ru>
2025-01-06 17:35:17 +0300

    3

--- src/main/java/com/example/TheLinkTamer/controller/UrlController.java
+++ src/main/java/com/example/TheLinkTamer/controller/UrlController.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/urls")
 public class UrlController {
     private final UrlService urlService;
     private final UserService userService;
@@ -34,9 +35,10 @@ public class UrlController {
     }
 
     @GetMapping("/{shortUrl}")
-    public ResponseEntity<String> getOriginalUrl(@PathVariable String shortUrl) {
+    public ResponseEntity<Void> redirectToOriginalUrl(@PathVariable String shortUrl) {
         return urlService.getOriginalUrl(shortUrl)
-                .map(ResponseEntity::ok)
+                .map(originalUrl -> ResponseEntity.status(302).location(URI.create(originalUrl)).<Void>build())
                 .orElse(ResponseEntity.notFound().build());
     }
+
 }
